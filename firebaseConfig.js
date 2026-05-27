e720cf9c632f3115297196b987346aee031bd66c
patilravi476-ctrl <patilravi476@gmail.com>
2026-05-27 15:57:23 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAe1d5vYyFKNPWa20OIdgLjGS3tP3Sz-GI",
+  authDomain: "cptc-fees-erp.firebaseapp.com",
+  projectId: "cptc-fees-erp",
+  storageBucket: "cptc-fees-erp.firebasestorage.app",
+  messagingSenderId: "647169866996",
+  appId: "1:647169866996:web:79be0c0e69d3efbdbfb2a1",
+  measurementId: "G-RC7B2Z52B0"
+};
+
+export function isFirebaseConfigured() {
+  return false;
+}
